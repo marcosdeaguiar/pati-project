574f79cfcb6ff0283b930b6255d9c454e34d5c98
Marcos de Aguiar <marcos.deaguiar@gmail.com>
2020-03-17 11:23:03 -0300

    Adding EFUtils to the project. This util currently only have a method to change the naming schema. This util changes the table / column naming schema to snake case.

---
+++ PatiEFUtils/src/DbContextNamingHelper.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+namespace Pati.EFUtils
+{
+    /// <summary>
+    /// Methods to change the default table naming in Entity Framework Core.
+    /// </summary>
+    class DbContextNamingHelper
+    {
+        /// <summary>
+        /// Changes the naming schema from default Entity Framework Core to
+        /// snake case. I.e "ColumnName" now is column_name.
+        /// </summary>
+        /// <param name="modelBuilder">The DbContext model builder to have the naming changed.</param>
+        public static void NamesToSnakeCase(ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                // Replace table names
+                entity.Relational().TableName = ToSnakeCase(entity.Relational().TableName);
+
+                // Replace column names
+                foreach (var property in entity.GetProperties())
+                {
+                    property.Relational().ColumnName = ToSnakeCase(property.Name);
+                }
+
+                foreach (var key in entity.GetKeys())
+                {
+                    key.Relational().Name = ToSnakeCase(key.Relational().Name);
+                }
+
+                foreach (var key in entity.GetForeignKeys())
+                {
+                    key.Relational().Name = ToSnakeCase(key.Relational().Name);
+                }
+
+                foreach (var index in entity.GetIndexes())
+                {
+                    index.Relational().Name = ToSnakeCase(index.Relational().Name);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Helper method with the regex to change to snake case.
+        /// </summary>
+        /// <param name="input">Name to be changed to snake case.</param>
+        /// <returns>Snake case version of the name.</returns>
+        private static string ToSnakeCase(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            var startUnderscores = Regex.Match(input, @"^_+");
+            return startUnderscores + Regex.Replace(input, @"([a-z0-9])([A-Z])", "$1_$2").ToLower();
+        }
+    }
+}
--- copypackagestorelease.bat
+++ copypackagestorelease.bat
@@ -3,3 +3,4 @@ copy PatiMiddleware\src\bin\Release\*.nupkg Release\
 copy PatiValidationCore\src\bin\Release\*.nupkg Release\
 copy PatiValidationService\src\bin\Release\*.nupkg Release\
 copy PatiValidators\src\bin\Release\*.nupkg Release\
+copy PatiEFUtils\src\bin\Release\*.nupkg Release\
