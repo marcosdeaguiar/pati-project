8e78f38a10560ff01eac65d623c87e1c1a2a9fc9
Marcos de Aguiar <marcos.deaguiar@gmail.com>
2020-03-31 15:47:58 -0300

    Adding jwt cookie middleware.

---
+++ PatiMiddleware/src/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pati.Middleware
+{
+    /// <summary>
+    /// Constants to be used throughout the library.
+    /// </summary>
+    static class Constants
+    {
+        public const string JwtTokenKey = "JWT_TOKEN";
+    }
+}
---
+++ PatiMiddleware/src/JwtCookieMiddleware.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Pati.Middleware
+{
+    /// <summary>
+    /// Middleware that gets jwt as cookie and sets the user for the application.
+    /// </summary>
+    public static class JwtCookieMiddleware
+    {
+        /// <summary>
+        /// Checks the jwt cookie and sets the user for the application.
+        /// </summary>
+        /// <param name="app">App builder reference.</param>
+        /// <param name="key">The key to decrypt the token.</param>
+        /// <param name="cookieName">Name of the cookie where the jwt is (defaults to jwt).</param>
+        public static void UseJwtCookieMiddleware(this IApplicationBuilder app, byte[] key, string cookieName = "jwt")
+        {
+            app.Use(async (context, next) =>
+            {
+                string jwtStr = context.Request.Cookies[cookieName];
+
+                if (string.IsNullOrEmpty(jwtStr))
+                {
+                    await next();
+                    return;
+                }
+
+                var validationParameters = new TokenValidationParameters
+                {
+                    // Clock skew compensates for server time drift.
+                    ClockSkew = TimeSpan.FromMinutes(5),
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    RequireSignedTokens = true,
+                    RequireExpirationTime = true,
+                    ValidateLifetime = true,
+                    // Ensure the token audience matches our audience value (default true):
+                    ValidateAudience = false,                    
+                    ValidateIssuer = false
+                };
+
+                try
+                {
+                    var claimsPrincipal = new JwtSecurityTokenHandler()
+                                              .ValidateToken(jwtStr,
+                                                             validationParameters,
+                                                             out var rawValidatedToken);
+                    //rawValidatedToken.
+                    context.User = claimsPrincipal;
+
+                    var jwtToken = rawValidatedToken as JwtSecurityToken;
+                    context.Items[Constants.JwtTokenKey] = jwtToken;
+                }
+                catch (Exception)
+                {
+                    // TODO: Log..
+                }
+
+                await next();
+            });
+        }
+    }
+}
