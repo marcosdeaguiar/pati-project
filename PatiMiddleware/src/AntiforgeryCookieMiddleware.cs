f687a418c7281fe39f52cb50975b7638a606c7a5
Marcos de Aguiar <marcos.deaguiar@gmail.com>
2020-05-18 17:02:22 -0300

    Fixed csrf authentication issue.

--- PatiMiddleware/src/AntiforgeryCookieMiddleware.cs
+++ PatiMiddleware/src/AntiforgeryCookieMiddleware.cs
@@ -15,27 +15,17 @@ namespace Pati.Middleware
         /// </summary>
         /// <param name="app">Application builder instance.</param>
         /// <param name="antiforgery">Antiforgery service.</param>
-        /// <param name="rootSegment">Root segment that will have the cookie. Defaults to "/".</param>
-        /// <param name="bundleFile">Html file name that contains the SPA application.</param>
         /// <param name="cookieName">Name of the cookie where the token will be added.</param>
         public static void UseAntiforgeryCookieMiddleware(this IApplicationBuilder app,
                                                           IAntiforgery antiforgery,
-                                                          string rootSegment = "/",
-                                                          string bundleFile="index.html",
                                                           string cookieName = "XSRF-TOKEN")
         {
             app.Use(async (context, next) =>
             {
-                string path = context.Request.Path.Value;
-
-                if (string.Equals(path, rootSegment, StringComparison.OrdinalIgnoreCase) ||
-                    string.Equals(path, rootSegment + bundleFile, StringComparison.OrdinalIgnoreCase))
-                {
-                    var tokens = antiforgery.GetAndStoreTokens(context);
-                    context.Response.Cookies.Append(cookieName,
-                                                    tokens.RequestToken,
-                                                    new CookieOptions() { HttpOnly = false });
-                }
+                var tokens = antiforgery.GetAndStoreTokens(context);
+                context.Response.Cookies.Append(cookieName,
+                                                tokens.RequestToken,
+                                                new CookieOptions() { HttpOnly = false });
 
                 await next();
             });
