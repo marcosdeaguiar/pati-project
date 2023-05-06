e265e451918fbd084dd68a390687ec8570bf41c5
Marcos de Aguiar <marcos.deaguiar@gmail.com>
2023-05-05 21:05:49 -0300

    Adding DateTime converters.

---
+++ PatiEFUtils/src/Converters/DateTimeLocalConverter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+
+namespace Pati.EFUtils.Converters
+{
+    /// <summary>
+    /// Makes sure that DateTime is converted to Local before sending to database.
+    /// </summary>
+    public class DateTimeLocalConverter : ValueConverter<DateTime, DateTime>
+    {
+        public DateTimeLocalConverter()
+            : base(
+                  src => src.Kind == DateTimeKind.Local ? src : DateTime.SpecifyKind(src, DateTimeKind.Local),
+                  dst => dst.Kind == DateTimeKind.Local ? dst : DateTime.SpecifyKind(dst, DateTimeKind.Local))
+        {
+        }
+    }
+}
---
+++ PatiEFUtils/src/Converters/DateTimeUtcConverter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+
+namespace Pati.EFUtils.Converters
+{
+    /// <summary>
+    /// Makes sure that DateTime is converted to UTC before sending to database.
+    /// </summary>
+    public class DateTimeUtcConverter : ValueConverter<DateTime, DateTime>
+    {
+        public DateTimeUtcConverter()
+            : base(
+                  src => src.Kind == DateTimeKind.Utc ? src : DateTime.SpecifyKind(src, DateTimeKind.Utc),
+                  dst => dst.Kind == DateTimeKind.Utc ? dst : DateTime.SpecifyKind(dst, DateTimeKind.Utc))
+        {
+        }
+    }
+}
