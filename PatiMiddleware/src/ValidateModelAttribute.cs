43be22a7c7b4eb49c91ab3fbb983a20e97cfcef2
Marcos de Aguiar <marcos.deaguiar@gmail.com>
2024-04-21 14:32:22 -0300

    Validation now sends ValidationProblems format.

--- PatiMiddleware/src/ValidateModelAttribute.cs
+++ PatiMiddleware/src/ValidateModelAttribute.cs
@@ -9,10 +9,27 @@ namespace Pati.Middleware
     /// </summary>
     public class ValidateModelAttribute : ActionFilterAttribute
     {
+        private readonly bool _sendValProblems;
+        
+        /// <summary>
+        /// Constructor for validate model attribute.
+        /// </summary>
+        /// <param name="sendValProblems">Send validation problems in ValidationProblems format.</param>
+        public ValidateModelAttribute(bool sendValProblems = true)
+        {
+            _sendValProblems = sendValProblems;
+        }
+        
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             if (!context.ModelState.IsValid)
             {
+                if (_sendValProblems)
+                {
+                    context.Result = new BadRequestObjectResult(new ValidationProblemDetails(context.ModelState));
+                    return;
+                }
+                
                 var errors = ValidationUtil.GetValidationErrorsFromModelState(context.ModelState);
                 context.Result = new BadRequestObjectResult(errors);
             }
