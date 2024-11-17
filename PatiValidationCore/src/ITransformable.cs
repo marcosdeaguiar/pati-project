0a84e72f5698b1628422e6477e16466c2d629a4f
Marcos de Aguiar <marcos.deaguiar@gmail.com>
2024-11-17 16:00:17 -0300

    Added ITransformable interface.

---
+++ PatiValidationCore/src/ITransformable.cs
@@ -0,0 +1,13 @@
+﻿namespace Pati.Validation.Core
+{
+    /// <summary>
+    /// Interface to transform entity before validation.
+    /// </summary>
+    public interface ITransformable
+    {
+        /// <summary>
+        /// Method called to transform internal data of a request or view model.
+        /// </summary>
+        void Transform();
+    }
+}
